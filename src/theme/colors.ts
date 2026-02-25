076b321864f7c8fda54d440a621bc3e02f1290b1
mateolorenzo <mateolorenzo.dev@gmail.com>
2026-02-25 00:40:47 -0300

    Feat: Implemented colors pallete

---
+++ src/theme/colors.ts
@@ -0,0 +1,24 @@
+export const colors = {
+  accent: {
+    mainBlue: "#001858",
+    mainRed: "#DF0024",
+    skyblue: "#67C9F5",
+    lightBackground: "#FAF1E3",
+  },
+  deco: {
+    decoYellow: "#EB9F00",
+    decoViolet: "#9F459A",
+    decoGreen: "#176F39",
+    decoBlue: "#0063A2",
+  },
+  neutral: {
+    300: "#333333",
+    500: "#555555",
+    700: "#777777",
+    white: "#FFFFFF",
+    black: "#222222",
+    disabled: "#D0D0D0",
+  },
+  error: "#811123",
+  transparent: "transparent",
+} as const;
