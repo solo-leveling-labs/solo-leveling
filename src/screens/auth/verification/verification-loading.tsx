1193d6363af130a99ef8f075a811b939c3f6ba01
mateolorenzo <mateolorenzo.dev@gmail.com>
2026-03-05 18:00:07 -0300

    Feat: Removed images from cache onSuccess

--- src/screens/auth/verification/verification-loading.tsx
+++ src/screens/auth/verification/verification-loading.tsx
@@ -3,7 +3,10 @@ import Buho from "@/assets/svg/buho.svg";
 import { useValidateIdentity } from "@/src/api/auth/auth.hooks";
 import { colors } from "@/src/theme/colors";
 import { fonts } from "@/src/theme/fonts";
-import { getIdentityPhotoUri } from "@/src/utils/identity-photo-storage";
+import {
+  deleteIdentityPhotos,
+  getIdentityPhotoUri,
+} from "@/src/utils/identity-photo-storage";
 import { useRouter } from "expo-router";
 import React, { useCallback, useEffect } from "react";
 import { StyleSheet, Text, View, useWindowDimensions } from "react-native";
@@ -52,8 +55,14 @@ const VerificationLoadingScreen = () => {
     validateIdentity(
       { frontalImageUri, rightProfileImageUri, leftProfileImageUri },
       {
-        onSuccess: () => completeAndNavigate("/verification-success"),
-        onError: () => completeAndNavigate("/verification-error"),
+        onSuccess: () => {
+          deleteIdentityPhotos();
+          completeAndNavigate("/verification-success");
+        },
+        onError: () => {
+          deleteIdentityPhotos();
+          completeAndNavigate("/verification-error");
+        },
       },
     );
   }, []);
